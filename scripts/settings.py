be371cda60b3fc104c29f9be7ce3859cb52990a5
cowfox <foxshee@gmail.com>
2023-08-03 20:22:54 -0700

    ✨ feat(Extension): Optimize the extension code
    - Turn UI elements to "Chinese/English"
    - Add support to use "sub-folder" to manage tag files
    - Fix issue when "randomly" select `n` tags to avoid duplication
    - Fix issues of "Settings"
    - Re-write the code for better performance / readability

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,6 +1,13 @@
 from modules import script_callbacks, shared
 
+# ----------------------------------
+# region Setting Options
+
 def on_ui_settings():
-    shared.opts.add_option("eps_enable_save_raw_prompt_to_pnginfo", shared.OptionInfo(False, "元プロンプトを pngninfo に保存する", section=("easy_prompt_selector", "EasyPromptSelector")))
+    shared.opts.add_option("eps_enable_save_raw_prompt_to_png_info",
+                           shared.OptionInfo(False, "保存原始提示词到图片信息/Save original prompt to png info",
+                                             section=("easy_prompt_selector", "EasyPromptSelector")))
+
+# endregion
 
 script_callbacks.on_ui_settings(on_ui_settings)
